a3f44aae5cd21c72945f4b9bf31ce6bc82e63f95
Daniele Davì <ddavi@MacBook-di-DD.local>
2014-09-09 17:39:05 +0200

    WCF Tutotial part 1-3
    A .Net Remoting Service example (and client to try it).
    An Asp.NET Web Service
    A WCF Service with 2 endpoint (http and tcp)

---
+++ IHelloRemotingService/HelloRemotingServiceClient/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace HelloRemotingServiceClient
+{
+    public partial class Form1 : Form
+    {
+
+        IHelloRemotingService.IhelloRemotingService client;
+
+        public Form1()
+        {
+            InitializeComponent();
+            TcpChannel channel = new TcpChannel();
+            ChannelServices.RegisterChannel(channel);
+            client = (IHelloRemotingService.IhelloRemotingService)Activator.GetObject(typeof(IHelloRemotingService.IhelloRemotingService), "tcp://localhost:8080/GetMessage");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = client.GetMessage(textBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ IHelloRemotingService/IHelloRemotingService/IhelloRemotingService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IHelloRemotingService
+{
+    public interface IhelloRemotingService
+    {
+        string GetMessage(string name);
+    }
+}
